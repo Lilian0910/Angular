c2ba9c441fd9ba4cae4797f71408f7a03c02e85e
Lilian0910 <Lilian0910@users.noreply.github.com>
2021-06-28 14:54:12 -0400

    git commit

--- reactapp/src/components/PostList.js
+++ reactapp/src/components/PostList.js
@@ -12,14 +12,21 @@ export class PostList extends Component {
 
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(response =>{console.log(response)})
+            .then(response =>{console.log(response)
+            this.setState({posts:response.data})
+            })
             .catch()
     }
 
     render() {
+        const {posts} = this.state
         return (
             <div>
                 Post List
+                {
+                    posts.length ?
+                    posts.map(post=>{<div key={post.id}></div>})
+                }
             </div>
         )
     }
